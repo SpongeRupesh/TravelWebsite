38563ee9dc90552feff08495c984b31b826b01f0
Rupesh Ghimire <ghimirerupesh90@gmail.com>
2025-12-18 14:28:53 +0545

    Add files via upload

---
+++ Travel Website/scrollreveal.js
@@ -0,0 +1,17 @@
+const sr = ScrollReveal({
+    distance: "25px",
+    duration: 1000,
+    easing: "ease-out",
+});
+
+// Base
+
+// Home
+
+// About
+
+// Destinations
+
+// Services
+
+// Subscribe
